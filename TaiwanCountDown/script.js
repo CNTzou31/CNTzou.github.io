58ebc959804640d4c7b6ee7f1e603f9d29bddd0f
CNTzou31 <146981983+CNTzou31@users.noreply.github.com>
2025-12-30 09:59:52 -0500

    Change Google Script URL in script.js
    Updated the Google Script URL for the countdown.

--- TaiwanCountDown/script.js
+++ TaiwanCountDown/script.js
@@ -1,7 +1,7 @@
 const TARGET_DATE = new Date('July 25, 2026 00:00:00').getTime();
 const USER_HEIGHT_CM = 173;
 // PASTE YOUR GOOGLE SCRIPT URL HERE
-const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbz2rn9s7PRwrnuLiMVWBGol12jnDiwQDHhifdR6r5EyHFAD9wnpQZfy0HX21m5czB15/exec';
+const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyfPk23oVfuQSwVKp_QtC2MuAbSKnvSl3xmLs6Ce5KcN7aTzL8kX7IC4r3fuJrsQ23K/exec';
 
 // State
 let currentDate = new Date(); // Updates with month navigation
